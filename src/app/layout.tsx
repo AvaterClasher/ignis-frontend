656bbdc84438828e91bcdaa0a4598b1c61d32d33
AvaterClasher <avater.clasher@gmail.com>
2025-07-07 13:57:21 +0530

    feat: setup react providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import QueryProvider from '@/providers/query-provider';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/layout/app-sidebar';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <main className="flex min-h-screen flex-1 flex-col p-6">
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </QueryProvider>
       </body>
     </html>
   );
