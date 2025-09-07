8bc12bc963d441f0c4a0579886f3ad6ce6c3a881
AvaterClasher <avater.clasher@gmail.com>
2025-09-07 21:19:54 +0530

    chore: expand layout metadata for Ignis project with additional Open Graph properties, author details, and updated robots settings; remove obsolete Open Graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,52 @@ export const metadata: Metadata = {
     siteName: 'Ignis',
     locale: 'en_US',
   },
+  creator: 'Soumyadip Moni',
+  publisher: 'Soumyadip Moni',
+  keywords: [
+    'Ignis',
+    'Code Execution',
+    'Code Sandbox',
+    'Code Runner',
+    'Code Executor',
+    'Code Sandbox',
+    'Code Runner',
+    'Code Executor',
+  ],
+  authors: [{ name: 'Soumyadip Moni', url: 'https://avater.tech' }],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ignis',
+    description: 'Secure Code Execution. Made by Soumyadip',
+    siteId: 'Avater004',
+    creator: '@Avater004',
+    creatorId: 'Avater004',
+    images: {
+      url: 'https://ignis.avater.tech/og.png',
+      alt: 'Ignis',
+    },
+  },
+  alternates: {
+    canonical: 'https://ignis.avater.tech',
+    types: { 'application/rss': 'https://ignis.avater.tech/rss' },
+  },
+  category: 'technology',
 };
 
 export default function DashboardLayout({
